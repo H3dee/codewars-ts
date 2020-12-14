4b7a547c69ed1be38f34ef90016acd2a9284242f
H3dee <62655938+H3dee@users.noreply.github.com>
2020-12-14 13:04:06 +0300

    Solved

--- task6/task6.ts
+++ task6/task6.ts
@@ -1,22 +1,49 @@
 /**Task: Sorting of array filled with class names
+ * Description: Need to sort input array by frequency of occurence
  * Example: ['header', 'menu', 'menu-item', 'menu-item', 'menu-item', 'footer', 'menu', 'link', 'link', 'link', 'link'] ->
- *          ['link', 'menu-item', 'menu', 'header', 'footer'] 
-*/
+ *          ['link', 'menu-item', 'menu', 'header', 'footer']
+ */
 
-interface className{
-      name: string,
-      amount: number
+interface className {
+  name: string
+  amount: number
 }
 
-
-function minimize():string[]
-function minimize(arr: string[]):string[]
-
-function minimize(arr?: string[]):string[]{
-      // const frequency: Set<T> = new Set
-
-      // arr.forEach( currentClass => {
-
-      // })
-      return []
+function findDuplicates<T>(arr: T[], needle: T): number {
+  return arr.reduce((acc, item) => (item === needle ? ++acc : acc), 0)
 }
+
+function minimize(): string[]
+function minimize(list: string[]): string[]
+
+function minimize(list?: string[]): string[] {
+  const frequency: Set<className> = new Set()
+
+  list.forEach((currentClass, _, arr) =>
+    frequency.add({
+      name: currentClass,
+      amount: findDuplicates(arr, currentClass),
+    })
+  )
+
+  return [...frequency]
+    .sort((a, b) => b.amount - a.amount)
+    .map((item) => item.name)
+    .filter((item, i, arr) => item !== arr[i - 1])
+}
+
+const classes: string[] = [
+  'header',
+  'menu',
+  'menu-item',
+  'menu-item',
+  'menu-item',
+  'footer',
+  'menu',
+  'link',
+  'link',
+  'link',
+  'link',
+]
+
+console.log(minimize(classes));
