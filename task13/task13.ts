d10969f466f8d71c90cd4175836d419e848e8f07
H3dee <62655938+H3dee@users.noreply.github.com>
2021-02-25 15:21:00 +0300

    task 13

--- task13/task13.ts
+++ task13/task13.ts
@@ -14,4 +14,16 @@ ls = []
 The corresponding sums are (put together in a list): [20, 20, 19, 16, 10, 0]
  */
 
-// export const partsSum = (ls: number[]): number[] =>
+export const partsSum = (ls: number[]): number[] => [
+    ...ls.reduce(
+        (acc) => {
+            acc.sums.push(acc.numbers.reduce((acc, value) => acc + value, 0));
+            acc.numbers.shift();
+            return acc;
+        },
+        { numbers: [...ls], sums: [] },
+    ).sums,
+    0,
+];
+
+console.log(partsSum([744125, 935, 407, 454, 430, 90, 144, 6710213, 889, 810, 2579358]));
